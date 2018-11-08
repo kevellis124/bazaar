591ac288dff307d0a4aab54df6d536fddf306dd5
Lauren Rose <larose@radford.edu>
2018-11-08 10:18:17 -0500

    switch to rails version of rubocop, fix all infractions

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,6 +2,6 @@
 
 # :nodoc:
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
-  layout 'mailer'
+  default from: "from@example.com"
+  layout "mailer"
 end
--- config/application.rb
+++ config/application.rb
@@ -1,15 +1,16 @@
 # frozen_string_literal: true
 
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Store
-  class Application < Rails::Application # :nodoc:
+  # :nodoc:
+  class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
