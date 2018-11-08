d8f663b838aadc5ef0a31c375a8988fa50fe2ff7
Will Krause <wkrause1@radford.edu>
2018-11-08 13:42:16 -0500

    Fixed merge conflicts

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,18 +1,9 @@
 # frozen_string_literal: true
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-<<<<<<< HEAD
-require 'spec_helper'
-require "selenium/webdriver"
-require 'capybara'
-require 'capybara/rspec'
-ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
-=======
 require "spec_helper"
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path("../config/environment", __dir__)
->>>>>>> 2862b5f1732b0af34ab8e420b80ee91c9894178e
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require "rspec/rails"
