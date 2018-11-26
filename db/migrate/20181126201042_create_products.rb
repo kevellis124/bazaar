223544c0de775708c618b826bbbbb08b81c1b172
Katheryn Osmond <katheryn.osmond@excella.com>
2018-11-26 20:38:20 +0000

    Created products model, factory, and migration

---
+++ db/migrate/20181126201042_create_products.rb
@@ -0,0 +1,12 @@
+class CreateProducts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :products do |t|
+      t.decimal :price
+      t.string :name
+      t.string :image
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/products.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :product do
+    price { "9.99" }
+    name { "MyString" }
+    image { "MyString" }
+    description { "MyString" }
+  end
+end
