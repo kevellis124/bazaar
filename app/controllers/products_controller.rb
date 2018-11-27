c18f7ed0abeb0a889a7e8e904322bc1dd6d11dc2
Will Krause <wkrause1@radford.edu>
2018-11-27 12:21:57 -0500

    Added URL to routes, added fields to form for new/update products. WIP for images

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,6 +5,7 @@ class ProductsController < ApplicationController
   # GET /products.json
   def index
     @products = Product.all
+
   end
 
   # GET /products/1
@@ -69,6 +70,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.fetch(:product, {})
+      params.require(:product).permit(:price, :name, :description)
     end
 end
